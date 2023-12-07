5f70ac71ab2bdd7ac5af2df67abdb98522d19fc2
Tod Kurt <todbot@users.noreply.github.com>
2023-12-07 09:16:33 -0800

    RP2040 USB Host Feather simpletest is now forwards to MIDI UART

--- examples/usb_host_midi_simpletest_rp2040usbfeather.py
+++ examples/usb_host_midi_simpletest_rp2040usbfeather.py
@@ -2,14 +2,16 @@
 #
 # SPDX-License-Identifier: Unlicense
 
-import audiopwmio
 import board
+import busio
 import synthio
 import adafruit_midi
 import adafruit_usb_host_midi
 import usb.core
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
+from adafruit_midi.control_change import ControlChange
+from adafruit_midi.pitch_bend import PitchBend
 
 print("Looking for midi device")
 raw_midi = None
@@ -21,25 +23,17 @@ while raw_midi is None:
         except ValueError:
             continue
 
-# This setup is for pin D4 on Feather RP2040 with USB Type A Host
-# You must wire this up yourself to an RC filter and headphones or line in of an amp
-audio = audiopwmio.PWMAudioOut(board.D4)
-synth = synthio.Synthesizer(sample_rate=22050)
-audio.play(synth)
+# This setup is to use TX pin on Feather RP2040 with USB Type A Host as MIDI out
+# You must wire up the needed resistors and jack yourself
+# This will forward all MIDI messages from the device to hardware uart MIDI
+uart = busio.UART(rx=board.RX, tx=board.TX, baudrate=31250, timeout=0.001)
 
-midi = adafruit_midi.MIDI(midi_in=raw_midi, in_channel=0)
+midi_device = adafruit_midi.MIDI(midi_in=raw_midi, in_channel=0)
+midi_uart = adafruit_midi.MIDI(midi_out=uart, midi_in=uart)
 
-pressed = {}
 
 while True:
-    msg = midi.receive()
-    if isinstance(msg, NoteOn) and msg.velocity != 0:
-        note = synthio.Note(synthio.midi_to_hz(msg.note))
-        print("noteOn: ", msg.note, "vel:", msg.velocity)
-        synth.press(note)
-        pressed[msg.note] = note
-    elif (isinstance(msg,NoteOff) or (isinstance(msg,NoteOn) and msg.velocity==0)) and msg.note in pressed:
-        print("noteOff:", msg.note, "vel:", msg.velocity)
-        note = pressed[msg.note]
-        synth.release(note)
-        del pressed[msg.note]
+    msg = midi_device.receive()
+    if msg:
+        print("midi msg:",msg)
+        midi_uart.send(msg)
