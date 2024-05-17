7b8a8534de413fccd2d57acee356a6bb183a2ce4
Tod Kurt <todbot@users.noreply.github.com>
2024-05-17 12:20:15 -0700

    pylint and black fixes

--- examples/usb_host_midi_simpletest.py
+++ examples/usb_host_midi_simpletest.py
@@ -5,12 +5,12 @@
 import audiobusio
 import board
 import synthio
-import adafruit_midi
-import adafruit_usb_host_midi
 import usb.core
+import wm8960
+import adafruit_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
-import wm8960
+import adafruit_usb_host_midi
 
 print("Looking for midi device")
 raw_midi = None
@@ -26,7 +26,9 @@ while raw_midi is None:
 # This setup is for the headphone output on the iMX RT 1060 EVK.
 dac = wm8960.WM8960(board.I2C())
 dac.start_i2s_out()
-audio = audiobusio.I2SOut(board.AUDIO_BCLK, board.AUDIO_SYNC, board.AUDIO_TXD, main_clock=board.AUDIO_MCLK)
+audio = audiobusio.I2SOut(
+    board.AUDIO_BCLK, board.AUDIO_SYNC, board.AUDIO_TXD, main_clock=board.AUDIO_MCLK
+)
 synth = synthio.Synthesizer(sample_rate=44100)
 audio.play(synth)
 
@@ -41,7 +43,9 @@ while True:
         print("noteOn: ", msg.note, "vel:", msg.velocity)
         synth.press(note)
         pressed[msg.note] = note
-    elif (isinstance(msg,NoteOff) or (isinstance(msg,NoteOn) and msg.velocity==0)) and msg.note in pressed:
+    elif (
+        isinstance(msg, NoteOff) or (isinstance(msg, NoteOn) and msg.velocity == 0)
+    ) and msg.note in pressed:
         print("noteOff:", msg.note, "vel:", msg.velocity)
         note = pressed[msg.note]
         synth.release(note)
--- examples/usb_host_midi_simpletest_rp2040usbfeather.py
+++ examples/usb_host_midi_simpletest_rp2040usbfeather.py
@@ -1,17 +1,17 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023 Scott Shawcroft for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
+# pylint: disable=unused-import
 
 import board
 import busio
-import synthio
-import adafruit_midi
-import adafruit_usb_host_midi
 import usb.core
+import adafruit_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
 from adafruit_midi.control_change import ControlChange
 from adafruit_midi.pitch_bend import PitchBend
+import adafruit_usb_host_midi
 
 print("Looking for midi device")
 raw_midi = None
@@ -35,5 +35,5 @@ midi_uart = adafruit_midi.MIDI(midi_out=uart, midi_in=uart)
 while True:
     msg = midi_device.receive()
     if msg:
-        print("midi msg:",msg)
+        print("midi msg:", msg)
         midi_uart.send(msg)
