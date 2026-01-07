4c4f63ca9e59864f7e7d2708b6be36328117ff32
foamyguy <foamyguy@gmail.com>
2026-01-07 16:00:56 -0600

    Add Fruit Jam example. use a timeout for USB host MIDI read.

--- examples/usb_host_midi_simpletest_fruitjam.py
+++ examples/usb_host_midi_simpletest_fruitjam.py
@@ -1,18 +1,21 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 Scott Shawcroft for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Tim Cocks for Adafruit Industries
 #
-# SPDX-License-Identifier: Unlicense
+# SPDX-License-Identifier: MIT
 
 import adafruit_midi
+import adafruit_tlv320
 import audiobusio
 import board
 import synthio
 import usb.core
-import wm8960
 from adafruit_midi.note_off import NoteOff
 from adafruit_midi.note_on import NoteOn
+from displayio import release_displays
+from pwmio import PWMOut
 
 import adafruit_usb_host_midi
 
+release_displays()
 print("Looking for midi device")
 raw_midi = None
 while raw_midi is None:
@@ -24,12 +27,20 @@ while raw_midi is None:
             continue
 
 
-# This setup is for the headphone output on the iMX RT 1060 EVK.
-dac = wm8960.WM8960(board.I2C())
-dac.start_i2s_out()
-audio = audiobusio.I2SOut(
-    board.AUDIO_BCLK, board.AUDIO_SYNC, board.AUDIO_TXD, main_clock=board.AUDIO_MCLK
-)
+mclk_pwm = PWMOut(board.I2S_MCLK, frequency=15_000_000, duty_cycle=2**15)
+
+i2c = board.I2C()
+dac = adafruit_tlv320.TLV320DAC3100(i2c)
+
+# set sample rate & bit depth, use bclk
+dac.configure_clocks(sample_rate=44100, bit_depth=16, mclk_freq=15_000_000)
+
+# use headphones
+dac.headphone_output = True
+dac.dac_volume = -5  # dB
+dac.headphone_volume = -30
+audio = audiobusio.I2SOut(board.I2S_BCLK, board.I2S_WS, board.I2S_DIN)
+
 synth = synthio.Synthesizer(sample_rate=44100)
 audio.play(synth)
 
